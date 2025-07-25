00c2760ae66d65ce19c2390c599eaa0483ab3085
r.grinev <r.grinev88@gmail.com>
2025-07-25 13:08:21 +0500

    Fix test

--- src/test/java/su/grinev/ObjectMapperTests.java
+++ src/test/java/su/grinev/ObjectMapperTests.java
@@ -1,14 +1,21 @@
 package su.grinev;
 
 import org.junit.jupiter.api.Test;
-import su.grinev.test.Command;
+import su.grinev.bson.BsonDeserializer;
+import su.grinev.bson.BsonWriter;
+import su.grinev.bson.Document;
 import su.grinev.test.VpnForwardPacketDto;
 import su.grinev.test.VpnRequestDto;
 
 import java.nio.ByteBuffer;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static su.grinev.test.Command.FOO;
 
 public class ObjectMapperTests {
 
@@ -16,7 +23,7 @@ public class ObjectMapperTests {
     public void serializeAndDeserializeObjectTest() {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        VpnRequestDto vpnRequestDto = VpnRequestDto.wrap(Command.FOO, VpnForwardPacketDto.builder()
+        VpnRequestDto vpnRequestDto = VpnRequestDto.wrap(FOO, VpnForwardPacketDto.builder()
                         .packet(new byte[1024])
                 .build());
 
@@ -29,47 +36,48 @@ public class ObjectMapperTests {
         assertEquals(vpnRequestDto, deserialized);
     }
 
-//    @Test
-//    public void performanceTest() {
-//        Binder binder = new Binder();
-//        BsonWriter bsonWriter = new BsonWriter(10, 1000, 10000);
-//        BsonDeserializer bsonDeserializer = new BsonDeserializer(10, 1000, 10000);
-//
-//        byte[] packet = new byte[128 * 1024];
-//
-//        List<String> test = new ArrayList<>();
-//        Random random = new Random();
-//
-//        for (int i = 0; i < 1000; i++) {
-//            test.add(Integer.toString(random.nextInt()));
-//        }
-//
-//        List<Long> serializationTime = new ArrayList<>();
-//        List<Long> deserializationTime = new ArrayList<>();
-//        Document deserialized = new Document(Map.of(), 0);
-//
-//
-//        for (int i = 0; i < 10000; i++) {
-//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(100000);
-//            Document documentMap = binder.unbind(request);
-//            long delta = System.nanoTime();
-//            bsonWriter.serialize(documentMap, outputStream);
-//            serializationTime.add((System.nanoTime() - delta) / 1000);
-//            byte[] data = outputStream.toByteArray();
-//            InputStream inputStream = new ByteArrayInputStream(data);
-//
-//            delta = System.nanoTime();
-//            deserialized = bsonDeserializer.deserialize(inputStream);
-//            deserializationTime.add((System.nanoTime() - delta) / 1000);
-//            Object request1 = binder.bind(VpnRequest.class, deserialized);
-//        }
-//        System.out.println(deserialized);
-//
-//        List<Long> sortedSerialization = serializationTime.stream().sorted().toList();
-//        List<Long> sortedDeserialization = deserializationTime.stream().sorted().toList();
-//
-//        System.out.println("Serialization median time: %sus".formatted(Long.toString(sortedSerialization.get(sortedSerialization.size() / 2))));
-//        System.out.println("Deserialization median time: %sus".formatted(Long.toString(sortedDeserialization.get(sortedDeserialization.size() / 2))));
-//    }
+    @Test
+    public void performanceTest() {
+        Binder binder = new Binder();
+        BsonWriter bsonWriter = new BsonWriter(10, 1000, 10000);
+        BsonDeserializer bsonDeserializer = new BsonDeserializer(10, 1000, 10000);
+
+        byte[] packet = new byte[128 * 1024];
+
+        List<String> test = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            test.add(Integer.toString(random.nextInt()));
+        }
+
+        VpnRequestDto<VpnForwardPacketDto> requestDto = VpnRequestDto.wrap(FOO, VpnForwardPacketDto.builder()
+                .packet(packet)
+                .build());
+
+        List<Long> serializationTime = new ArrayList<>();
+        List<Long> deserializationTime = new ArrayList<>();
+        Document deserialized = new Document(Map.of(), 0);
+        Object request1 = null;
+
+        for (int i = 0; i < 10000; i++) {
+            Document documentMap = binder.unbind(requestDto);
+            long delta = System.nanoTime();
+            ByteBuffer b = bsonWriter.serialize(documentMap);
+            serializationTime.add((System.nanoTime() - delta) / 1000);
+
+            delta = System.nanoTime();
+            deserialized = bsonDeserializer.deserialize(b);
+            deserializationTime.add((System.nanoTime() - delta) / 1000);
+            request1 = binder.bind(VpnRequestDto.class, deserialized);
+        }
+        System.out.println(deserialized);
+
+        List<Long> sortedSerialization = serializationTime.stream().sorted().toList();
+        List<Long> sortedDeserialization = deserializationTime.stream().sorted().toList();
+
+        System.out.println("Serialization median time: %sus".formatted(Long.toString(sortedSerialization.get(sortedSerialization.size() / 2))));
+        System.out.println("Deserialization median time: %sus".formatted(Long.toString(sortedDeserialization.get(sortedDeserialization.size() / 2))));
+    }
 
 }
